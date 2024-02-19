8db779cc3bd3d43a17c97d2e8b6fe3f38ede6bd4
Oliver Horvat <ohorvat@etfos.hr>
2024-02-19 03:48:42 +0100

    Initial commit pt5

---
+++ openCV/build/intermediates/cxx/Debug/6h3hq4m5/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Joco\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Joco\\AndroidStudioProjects\\AgeAgainstTheMachine\\openCV\\.cxx\\Debug\\6h3hq4m5\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/6h3hq4m5/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Joco\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Joco\\AndroidStudioProjects\\AgeAgainstTheMachine\\openCV\\.cxx\\Debug\\6h3hq4m5\\x86" ^
+  opencv_jni_shared
